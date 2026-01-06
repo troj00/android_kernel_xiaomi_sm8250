f219f2a79684106fc4f781588a97dbc4d1985b1e
troj00 <124354827+troj00@users.noreply.github.com>
2026-01-06 23:22:19 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -169,7 +169,7 @@ mkdir -p anykernel/kernels/
 if [ $NSU_ENABLE -eq 1 ]; then
     cd out/arch/arm64/boot/
 #    https://github.com/troj00/NextGenSU_KernelPatch_patch/releases/download/0.12.5/patch_linux
-    wget https://github.com/SukiSU-Ultra/SukiSU_KernelPatch_patch/releases/download/0.12.5/patch_linux
+    wget https://github.com/SukiSU-Ultra/SukiSU_KernelPatch_patch/releases/download/0.12.2/patch_linux
     chmod +x patch_linux
     ./patch_linux
     rm Image
@@ -341,7 +341,7 @@ mkdir -p anykernel/kernels/
 # Patch for NextGenSU KPM support. 
 if [ $NSU_ENABLE -eq 1 ]; then
     cd out/arch/arm64/boot/
-    wget https://github.com/SukiSU-Ultra/SukiSU_KernelPatch_patch/releases/download/0.12.5/patch_linux
+    wget https://github.com/SukiSU-Ultra/SukiSU_KernelPatch_patch/releases/download/0.12.2/patch_linux
     chmod +x patch_linux
     ./patch_linux
     rm Image
